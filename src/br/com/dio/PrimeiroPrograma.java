aef5f5866accfe335b1321e7c333b9c78113ea75
DanielSS08 <dsilva.soares08@gmail.com>
2022-01-27 17:31:30 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,7 +9,7 @@ public class PrimeiroPrograma {
         System.out.println(gato);
 
 
-        /* teste int a = 5;
+        /* int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
     }
